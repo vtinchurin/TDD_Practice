8aa23db2ec7c60c524969bc638b650137ba0904f
User <vladislav.tinchurin@gmail.com>
2024-07-16 17:19:22 +0300

    Task 10 complete

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,44 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button: Button
+    private lateinit var textView:TextView
+    private lateinit var layout: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        layout = findViewById(R.id.rootLayout)
+        textView = findViewById(R.id.titleTextView)
+        button = findViewById(R.id.removeButton)
+
+        button.setOnClickListener {
+            layout.removeView(textView)
+            button.isEnabled = false
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val removedTextView = layout.childCount ==1
+        outState.putBoolean("textView",removedTextView)
+        outState.putBoolean("buttonEnabled",button.isEnabled)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val removedTextView = savedInstanceState.getBoolean("textView")
+        when(removedTextView){
+            true ->  layout.removeView(textView)
+            false -> {}
+
+        }
+        button.isEnabled = savedInstanceState.getBoolean("buttonEnabled")
     }
 }
