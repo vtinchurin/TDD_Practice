8e805e0ee144356abe2f7af0a2ee3951c8204645
User <vladislav.tinchurin@gmail.com>
2024-07-29 14:10:44 +0300

    Task 22 complete v1

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,46 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var arrayList: ArrayList<String>
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("qwe","Create")
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val bindings = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindings.root)
+        arrayList = if (savedInstanceState != null) {
+            savedInstanceState.getStringArrayList("list") as ArrayList<String>
+        }else arrayListOf()
+        for (i in arrayList){
+            val tv = TextView(this)
+            tv.text = i
+            bindings.contentLayout.addView(tv)
+        }
+
+
+
+        bindings.actionButton.setOnClickListener {
+            val textView = TextView(this)
+            textView.text = bindings.inputEditText.text.toString()
+            bindings.contentLayout.addView(textView)
+            arrayList.add(textView.text.toString())
+            bindings.inputEditText.setText("")
+        }
+
     }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.i("qwe","SaveInstance")
+        super.onSaveInstanceState(outState)
+        outState.putStringArrayList("list",arrayList)
+    }
+
+
 }
