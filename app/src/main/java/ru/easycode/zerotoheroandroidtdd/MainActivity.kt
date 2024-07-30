5bd8702122225e01a3a90c47c9308cd6cd1504ca
User <vladislav.tinchurin@gmail.com>
2024-07-30 13:53:16 +0300

    Task 22 complete final

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -9,38 +9,37 @@ import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var arrayList: ArrayList<String>
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.i("qwe","Create")
+        Log.i("qwe", "Create")
+        arrayList = ArrayList()
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        val bindings = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(bindings.root)
-        arrayList = if (savedInstanceState != null) {
-            savedInstanceState.getStringArrayList("list") as ArrayList<String>
-        }else arrayListOf()
-        for (i in arrayList){
-            val tv = TextView(this)
-            tv.text = i
-            bindings.contentLayout.addView(tv)
-        }
+        setContentView(binding.root)
 
-
-
-        bindings.actionButton.setOnClickListener {
+        binding.actionButton.setOnClickListener {
             val textView = TextView(this)
-            textView.text = bindings.inputEditText.text.toString()
-            bindings.contentLayout.addView(textView)
-            arrayList.add(textView.text.toString())
-            bindings.inputEditText.setText("")
+            textView.text = binding.inputEditText.text.toString()
+            binding.contentLayout.addView(textView)
+            arrayList.add(binding.inputEditText.text.toString())
+            binding.inputEditText.setText("")
         }
-
     }
-
 
     override fun onSaveInstanceState(outState: Bundle) {
-        Log.i("qwe","SaveInstance")
+        Log.i("qwe", "SaveInstance")
         super.onSaveInstanceState(outState)
-        outState.putStringArrayList("list",arrayList)
+        outState.putStringArrayList("list", arrayList)
     }
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        arrayList = savedInstanceState.getStringArrayList("list") as ArrayList<String>
+        for (i in arrayList) {
+            val tv = TextView(this)
+            tv.text = i
+            binding.contentLayout.addView(tv)
+        }
+    }
 
 }
